a9d5ca7bb197b403cb32ff82a28d18b4e88cc185
modkoreknight <60253359+modkoreknight@users.noreply.github.com>
2026-08-24 11:56:03 -1000

    Add files via upload

---
+++ GeneralLedgerConsolidation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+using Interact.BusinessLogic;
+using Interact.Common;
+
+namespace Interact.UserInterface
+{
+    static class Program
+    {
+        public static String UserName;
+
+        //Data comes from SerialNo in App.config
+        public static Settings Settings;
+
+        //User settings only
+        public static Branch Branch;
+
+        public static Boolean IsReadOnly;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Program.Settings = Settings.CreateSettings(Properties.Settings.Default.ApplicationKey);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new MainForm());
+        }
+    }
+}
