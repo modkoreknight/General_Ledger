0ce31d2314f91e1bda0f0e95e410d8ea5b2d45b6
modkoreknight <60253359+modkoreknight@users.noreply.github.com>
2026-08-24 11:51:01 -1000

    Add files via upload

---
+++ KeyGenerator/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace KeyGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtApplicationKey = new System.Windows.Forms.TextBox();
+            this.lblApplicationKey = new System.Windows.Forms.Label();
+            this.txtMachineCode = new System.Windows.Forms.TextBox();
+            this.lblMachineCode = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.cmbBranch = new System.Windows.Forms.ComboBox();
+            this.lblBranch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(225, 170);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // txtApplicationKey
+            // 
+            this.txtApplicationKey.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApplicationKey.Location = new System.Drawing.Point(117, 106);
+            this.txtApplicationKey.Name = "txtApplicationKey";
+            this.txtApplicationKey.Size = new System.Drawing.Size(300, 21);
+            this.txtApplicationKey.TabIndex = 5;
+            // 
+            // lblApplicationKey
+            // 
+            this.lblApplicationKey.AutoSize = true;
+            this.lblApplicationKey.Location = new System.Drawing.Point(28, 114);
+            this.lblApplicationKey.Name = "lblApplicationKey";
+            this.lblApplicationKey.Size = new System.Drawing.Size(83, 13);
+            this.lblApplicationKey.TabIndex = 4;
+            this.lblApplicationKey.Text = "Application key:";
+            // 
+            // txtMachineCode
+            // 
+            this.txtMachineCode.Location = new System.Drawing.Point(117, 69);
+            this.txtMachineCode.Name = "txtMachineCode";
+            this.txtMachineCode.Size = new System.Drawing.Size(300, 21);
+            this.txtMachineCode.TabIndex = 3;
+            // 
+            // lblMachineCode
+            // 
+            this.lblMachineCode.AutoSize = true;
+            this.lblMachineCode.Location = new System.Drawing.Point(35, 77);
+            this.lblMachineCode.Name = "lblMachineCode";
+            this.lblMachineCode.Size = new System.Drawing.Size(76, 13);
+            this.lblMachineCode.TabIndex = 2;
+            this.lblMachineCode.Text = "Machine code:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(144, 170);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // cmbBranch
+            // 
+            this.cmbBranch.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBranch.FormattingEnabled = true;
+            this.cmbBranch.Location = new System.Drawing.Point(117, 32);
+            this.cmbBranch.Name = "cmbBranch";
+            this.cmbBranch.Size = new System.Drawing.Size(300, 21);
+            this.cmbBranch.Sorted = true;
+            this.cmbBranch.TabIndex = 1;
+            // 
+            // lblBranch
+            // 
+            this.lblBranch.AutoSize = true;
+            this.lblBranch.Location = new System.Drawing.Point(67, 40);
+            this.lblBranch.Name = "lblBranch";
+            this.lblBranch.Size = new System.Drawing.Size(44, 13);
+            this.lblBranch.TabIndex = 0;
+            this.lblBranch.Text = "Branch:";
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnGenerate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(444, 224);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtApplicationKey);
+            this.Controls.Add(this.lblApplicationKey);
+            this.Controls.Add(this.txtMachineCode);
+            this.Controls.Add(this.lblMachineCode);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.cmbBranch);
+            this.Controls.Add(this.lblBranch);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Key Generator...";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtApplicationKey;
+        private System.Windows.Forms.Label lblApplicationKey;
+        private System.Windows.Forms.TextBox txtMachineCode;
+        private System.Windows.Forms.Label lblMachineCode;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ComboBox cmbBranch;
+        private System.Windows.Forms.Label lblBranch;
+
+    }
+}
+
---
+++ KeyGenerator/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Interact.BusinessLogic;
+
+namespace KeyGenerator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            foreach (String str in Enum.GetNames(typeof(Branch)))
+            {
+                cmbBranch.Items.Add(Utility.EnumDecode(str));
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            switch (button.Name)
+            {
+                case "btnGenerate":
+                    Branch branch = (Branch)Enum.Parse(typeof(Branch), Utility.EnumEncode(cmbBranch.SelectedItem.ToString()));
+                    String machineCode = txtMachineCode.Text;
+                    //Machine code should be at least 8 characters long...
+                    String salt = branch.ToString() + "1n+3r@c+" + machineCode.Substring((machineCode.Length / 2) - 4, 8);
+                    String applicationKey = AESCryptography.CreatePasswordHash(machineCode, salt);
+                    applicationKey = applicationKey.Insert(10, ((Int32)branch).ToString("D3"));
+                    if (branch == Branch.Area_Supervisor_ || branch == Branch.Verifier_ || branch == Branch.HO_)
+                    {
+                        applicationKey = applicationKey.Insert(23, "X");
+                    }
+                    else
+                    {
+                        applicationKey = applicationKey.Insert(23, "Y");
+                    }
+                    txtApplicationKey.Text = applicationKey;
+                    break;
+                case "btnClose":
+                    DialogResult drExit = MessageBox.Show("Are you sure you want to quit?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+                    if (drExit == DialogResult.Yes)
+                    {
+                        this.Close();
+                        this.Dispose();
+                    }
+                    break;
+            }
+        }
+    }
+}
